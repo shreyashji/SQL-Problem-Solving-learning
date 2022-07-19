52537be2bfaa35dbff5aecae08de2628aea9c738
Shreyash Choudhary <shreyashchoudhary7789@gmail.com>
2022-07-19 22:51:36 +0530

    adding sql problem

---
+++ SQL interview some of the questions would be definitely from below list/146-Team size problem.sql
@@ -0,0 +1,29 @@
+use it;
+
+create table team_size (
+employee_id int,
+team_id int
+);
+insert into team_size values (1,8);
+insert into team_size values (2,8);
+insert into team_size values (3,8);
+insert into team_size values (4,7);
+insert into team_size values (5,9);
+insert into team_size values (6,9);
+
+select * from team_size;
+
+#1:- Window Functions (Most Simpler Soln)
+SELECT employee_id,COUNT(employee_id) OVER(PARTITION BY  team_id ) AS team_size
+FROM team_size ORDER BY employee_id;
+
+#2:- Self Join
+SELECT e1.employee_id,COUNT(*) AS team_size 
+FROM team_size e1 JOIN team_size e2 ON
+e1.team_id = e2.team_id
+GROUP BY e1.employee_id ORDER BY e1.employee_id;
+
+#3:- Using Correlated SubQuery
+SELECT employee_id,
+(SELECT COUNT(1) FROM team_size e2 WHERE E2.team_id = e.team_id ) as teaam_sizee
+from team_size e
